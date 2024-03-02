e30366535cd2bcaa5b75e6b0fa926920cd27a9d8
DavidBG73 <98876381+dbelgar73@users.noreply.github.com>
2024-03-02 11:12:39 +0100

    Add files via upload

---
+++ p2/SAR_p2_cuenta_palabras_plantilla-1.py
@@ -0,0 +1,139 @@
+#! -*- encoding: utf8 -*-
+
+## Nombres: 
+
+########################################################################
+########################################################################
+###                                                                  ###
+###  Todos los métodos y funciones que se añadan deben documentarse  ###
+###                                                                  ###
+########################################################################
+########################################################################
+
+import argparse
+import os
+import re
+from typing import Optional
+
+def sort_dic_by_values(d:dict) -> list:
+    return sorted(d.items(), key=lambda a: (-a[1], a[0]))
+
+class WordCounter:
+
+    def __init__(self):
+        """
+           Constructor de la clase WordCounter
+        """
+        self.clean_re = re.compile('\W+')
+
+    def write_stats(self, filename:str, stats:dict, use_stopwords:bool, full:bool):
+        """
+        Este método escribe en fichero las estadísticas de un texto
+            
+        :param 
+            filename: el nombre del fichero destino.
+            stats: las estadísticas del texto.
+            use_stopwords: booleano, si se han utilizado stopwords
+            full: boolean, si se deben mostrar las stats completas
+        """
+
+        with open(filename, 'w', encoding='utf-8', newline='\n') as fh:
+            #CONTAR LINEAS Y PALABRAS
+            lines = fh.readlines()
+            num_lineas = 0
+            num_palabras = 0
+            for line in lines: 
+                num_lineas+=1
+                words = line.split()
+                for word in words:
+                    num_palabras+=1
+            pass
+        
+        
+
+    def file_stats(self, fullfilename:str, lower:bool, stopwordsfile:Optional[str], bigrams:bool, full:bool):
+        """
+        Este método calcula las estadísticas de un fichero de texto
+
+        :param 
+            fullfilename: el nombre del fichero, puede incluir ruta.
+            lower: booleano, se debe pasar todo a minúsculas?
+            stopwordsfile: nombre del fichero con las stopwords o None si no se aplican
+            bigram: booleano, se deben calcular bigramas?
+            full: booleano, se deben montrar la estadísticas completas?
+        """
+
+        stopwords = [] if stopwordsfile is None else open(stopwordsfile, encoding='utf-8').read().split()
+
+        # variables for results
+
+        sts = {
+                'nwords': 0,
+                'nlines': 0,
+                'word': {},
+                'symbol': {},
+                'prefix': {},
+                'suffix': {}
+                }
+
+        if bigrams:
+            sts['biword'] = {}
+            sts['bisymbol'] = {}
+
+        # COMPLETAR 
+        # contar lineas
+        # contar palabras    
+        # AYUDA: line = self.clean_re.sub(' ', line)
+
+        filename, ext0 = os.path.splitext(fullfilename)
+        opciones = {}
+        if lower: opciones.append("l")
+        if not (stopwordsfile == None): opciones.append("s") 
+        if bigrams: opciones.append("b")
+        if full: opciones.append("f")
+        new_filename = fullfilename + "_" + opciones + "stats"
+        self.write_stats(new_filename, sts, stopwordsfile is not None, full)
+
+
+    def compute_files(self, filenames:str, **args):
+        """
+        Este método calcula las estadísticas de una lista de ficheros de texto
+
+        :param 
+            filenames: lista con los nombre de los ficheros.
+            args: argumentos que se pasan a "file_stats".
+
+        :return: None
+        """
+
+        for filename in filenames:
+            self.file_stats(filename, **args)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Compute some statistics from text files.')
+    parser.add_argument('file', metavar='file', type=str, nargs='+',
+                        help='text file.')
+
+    parser.add_argument('-l', '--lower', dest='lower',
+                        action='store_true', default=False, 
+                        help='lowercase all words before computing stats.')
+
+    parser.add_argument('-s', '--stop', dest='stopwords', action='store',
+                        help='filename with the stopwords.')
+
+    parser.add_argument('-b', '--bigram', dest='bigram',
+                        action='store_true', default=False, 
+                        help='compute bigram stats.')
+
+    parser.add_argument('-f', '--full', dest='full',
+                        action='store_true', default=False, 
+                        help='show full stats.')
+
+    args = parser.parse_args()
+    wc = WordCounter()
+    wc.compute_files(args.file,
+                     lower=args.lower,
+                     stopwordsfile=args.stopwords,
+                     bigrams=args.bigram,
+                     full=args.full)
